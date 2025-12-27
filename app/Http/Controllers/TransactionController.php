c2adcb82a4c721c62cb62b30b2b6e6a9ed260fc9
Munge-flip <ggonzales2@ssct.edu.ph>
2025-12-27 11:55:06 +0800

    commit

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Transaction;
 use Illuminate\Http\Request;
 
 class TransactionController extends Controller
@@ -13,5 +14,28 @@ class TransactionController extends Controller
     public function create()
     {
         return view('transactions.create');
+    }
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'date' => ['required', 'date'],
+            'time_period' => ['required', 'string'],
+            'type' => ['required', 'string'],
+            'amount' => ['required', 'numeric', 'min:0'],
+            'status' => ['required', 'string'],
+        ]);
+
+        $fee = 0;
+
+        Transaction::create([
+            'date' => $validated['date'],
+            'time_period' => $validated['time_period'],
+            'type' => $validated['type'],
+            'amount' => $validated['amount'],
+            'status' => $validated['status'],
+            'fee' => $fee,
+        ]);
+
+        return redirect()->route('transactions.index');
     }
 }
